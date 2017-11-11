9a9bffa44df4b2806dfdafc962d3df82ed9759a8
Rajkumar <myidrajkumar@gmail.com>
2017-11-11 21:28:46 +0530

    Added more

---
+++ src/main/java/com/rajkumar/java/utils/collections/FactoryMethodsInCollections.java
@@ -0,0 +1,118 @@
+package com.rajkumar.java.utils.collections;
+
+import com.rajkumar.java.utils.lib.Utils;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Factory methods to initialize collections. 
+ * 1. They are structurally immutable. Elements cannot be added, removed, or replaced. 
+ *    If we try, always UnsupportedOperationException will be thrown. 
+ * 2. No null elements. Attempts to create them with null elements result in NullPointerException. 
+ * 3. For Lists, The list element order is same as the order of the provided arguments. 
+ *    For Sets, Unspecified 
+ * 4. For Sets, will reject duplicate elements at creation time. 
+ *    If we try to add to existing set, throws IllegalArgumentException. 
+ * 5. They are serializable if all elements are serializable.
+ * 
+ * @author Rajkumar
+ *
+ */
+public class FactoryMethodsInCollections {
+  
+  private static Logger logger = LogManager.getLogger();
+  
+  /**
+   * Main Method.
+   * 
+   * @param args empty arguments
+   */
+  public static void main(String[] args) {
+    
+    displayList();
+    
+    // In sets and maps, already shown general characters are ignored
+    displaySet();
+    
+    displayMap();
+    
+  }
+  
+  /**
+   * Map characteristics.
+   */
+  private static void displayMap() {
+    
+    Map<Integer, String> immutableMap = Map.of(1, "Peter", 2, "Gerhard");
+    logger.info("Immutable Map = " + immutableMap);
+    
+    Map<Integer, String> mapEntries = Map.ofEntries(Map.entry(1, "Peter"),
+        Map.entry(2, "Gerhad"), Map.entry(3, "Philip"));
+    
+    logger.info("Immutable Map Entries= " + mapEntries);
+  }
+  
+  /**
+   * Set characteristics.
+   */
+  private static void displaySet() {
+    
+    try {
+      Set.of("Peter", "Philip", "Peter");
+    } catch (IllegalArgumentException exception) {
+      logger.info(Utils.getException(exception));
+      logger.info("Cannot add duplicate element in set at creation time");
+      // If you try to add, UnsupportedOperationException will be thrown.
+    }
+  }
+  
+  /**
+   * List characteristics.
+   */
+  private static void displayList() {
+    
+    // Creating empty list
+    List<String> emptyList = List.of();
+    
+    // Creating immutable list
+    List<String> immutableList = List.of("Peter", "Philip");
+    
+    logger.info("emptyList = " + emptyList);
+    logger.info("Immutable List = " + immutableList);
+    
+    // Try using null
+    try {
+      
+      List.of("Peter", null, "Philip");
+      
+    } catch (NullPointerException exception) {
+      logger.info(Utils.getException(exception));
+      logger.info("nulls are not allowed in List.of()");
+    }
+    
+    // Try adding an element to immutable list
+    try {
+      
+      immutableList.add("Gerhard");
+      
+    } catch (UnsupportedOperationException exception) {
+      logger.info(Utils.getException(exception));
+      logger.info("Cannot add an element to the immutable List");
+    }
+    
+    // Try removing an element from immutable List
+    try {
+      
+      immutableList.remove(1);
+    } catch (UnsupportedOperationException exception) {
+      logger.info(Utils.getException(exception));
+      logger.info("Cannot remove an element from immutable List");
+    }
+  }
+  
+}
---
+++ src/main/java/com/rajkumar/java/utils/designpattern/structure/FlyWeightFactoryDesignPatternMedium.java
@@ -0,0 +1,90 @@
+package com.rajkumar.java.utils.designpattern.structure;
+
+import java.util.HashMap;
+
+import org.apache.logging.log4j.LogManager;
+
+/**
+ * The Flyweight Pattern is a Structural Pattern. Its intent is to minimize
+ * memory usage. The intent of the Flyweight Pattern is to use sharing to
+ * support a large number of objects that have part of their internal state in
+ * common while the other part of state varies.
+ * 
+ * @author Rajkumar
+ *
+ */
+public class FlyWeightFactoryDesignPatternMedium {
+  
+  private interface Character {
+    
+    public void render();
+  }
+  
+  private static class CharacterA implements Character {
+    
+    private String color;
+    private int xPosition;
+    private int yPosition;
+    
+    public CharacterA(String color) {
+      
+      this.color = color;
+    }
+    
+    public void setX(int x) {
+      
+      this.xPosition = x;
+    }
+    
+    public void setY(int y) {
+      
+      this.yPosition = y;
+    }
+    
+    @Override
+    public void render() {
+      
+      LogManager.getLogger().info("Character at position " + xPosition + ", "
+          + yPosition + " with color " + color);
+    }
+    
+  }
+  
+  private static class CharacterFactory {
+    
+    private static HashMap<String, Character> characterMap = new HashMap<>();
+    
+    public static Character getCharacter(String color) {
+      
+      Character character = (Character) characterMap.get(color);
+      
+      if (character == null) {
+        character = new CharacterA(color);
+        characterMap.put(color, character);
+      }
+      
+      return character;
+    }
+    
+  }
+  
+  /**
+   * Main Method.
+   * 
+   * @param args empty arguments
+   */
+  public static void main(String[] args) {
+    
+    final String[] colors = {"Red", "Green", "Blue", "White", "Black"};
+    
+    for (int i = 0; i < 15; ++i) {
+      CharacterA character = (CharacterA) CharacterFactory
+          .getCharacter(colors[(int) (Math.random() * colors.length)]);
+      character.setX((int) (Math.random() * 100));
+      character.setY((int) (Math.random() * 100));
+      character.render();
+    }
+    
+  }
+  
+}
